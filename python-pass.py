1c8c0513d984b09cd728a594f2d9bfb5ba4b9e1e
sondoskareem <sondosabd94@gmail.com>
2021-09-30 22:54:40 +0300

    first task

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,51 @@
 
+import collections
 
 class Solution:
 
+    
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        pass 
+
+# Example 1:
+
+string1 = "babad"
+# print(string1[0:3])
+
+
+
+
+########################################
+
+
+
+# Example 2:
+string2 = "cbbd"
+duplicates =''
+for char in string2:
+  count = string2.count(char)
+  if count > 1:
+    duplicates += char
+# print(duplicates)
+
+
+
+
+
+
+
+########################################
+# Example 3:
+string3 = "a"
+# print(string3)
+   
+
+
+
+
+########################################
+# Example 4:
+string3 = "a"
+print(string3[0])
+
